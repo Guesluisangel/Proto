de8b535a6b8ec9a7d8102e71d93340dbba902a0b
luisbp <cheloluisito@gmail.com>
2022-02-26 12:12:25 -0600

    Avance de barra

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from "./componentes/Menu"
+import Carrusel from "./componentes/carrusel"
+import Contenido from "./componentes/contenido"
+
+function App() {
+  return (
+    <div className="container-fluid">
+     <Menu/>
+     <Carrusel/>
+     <Contenido/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/contenido.js
@@ -0,0 +1,47 @@
+function contenido() {
+  return (
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-4">
+            <div class="card">
+              <img src="/imagenes/halo.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Noticias</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="https://www.xbox.com/es-MX/games/halo-infinite" class="btn btn-primary">
+                  seguir leyendo...
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+          <div class="card">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+          </div>
+          <div class="col-4">
+          <div class="card">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default contenido;
